8195a75df02fae299f7e191a61a0f7f44c2bd31a
anwarbahida <anwar1bahida1@gmail.com>
2024-12-27 22:39:43 +0100

    Ajoute de photo de dimplome

--- src/medecin/medecin.module.ts
+++ src/medecin/medecin.module.ts
@@ -4,9 +4,11 @@ import { MedecinController } from './medecin.controller';
 import { MedecinService } from './medecin.service';
 import { medecin } from './medecin.entity';
 import { databaseConfig } from '../config/database.config';
+import { MulterModule } from '@nestjs/platform-express';
+
 
 @Module({
-  imports: [TypeOrmModule.forRoot(databaseConfig), TypeOrmModule.forFeature([medecin])],
+  imports: [TypeOrmModule.forRoot(databaseConfig), TypeOrmModule.forFeature([medecin]), MulterModule.register({dest: './uploads',}),],
   controllers: [MedecinController],
   providers: [MedecinService],
 })
